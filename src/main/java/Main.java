13d09cb9f4daf5aa20ad8870a7c524ef7f43afbe
Matus Caban <caban.matus@gmail.com>
2025-07-10 14:33:38 +0200

    move reading from file to QuestionManager

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,15 @@
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        QuestionManager qm = new QuestionManager();
+        qm.setQuestions("java");
 
-        List<Question> questions = new ArrayList<>();
+        List<Question> questions = qm.qetQuestions();
 
-        Gson gson = new Gson();
-        try (Reader reader = new FileReader("src/json/java.json")){
-            Type guestionListType = new TypeToken<List<Question>>(){}.getType();
-            questions = gson.fromJson(reader, guestionListType);
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-
-
-        for (Question q: questions){
+        for (Question q: questions) {
             q.printQuestion();
         }
+
     }
 }
--- src/main/java/QuestionManager.java
+++ src/main/java/QuestionManager.java
@@ -1,2 +1,34 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileReader;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuestionManager {
+    private List<Question> questions;
+
+    public QuestionManager(){
+        this.questions = new ArrayList<>();
+    }
+
+    public void setQuestions(String topics) {
+        Gson gson = new Gson();
+
+        try (Reader reader = new FileReader("src/json/" + topics + ".json")) {
+            Type questionListType = new TypeToken<List<Question>>(){}.getType();
+            this.questions = gson.fromJson(reader,questionListType);
+
+
+            reader.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public List<Question> qetQuestions(){
+        return this.questions;
+    }
 }
