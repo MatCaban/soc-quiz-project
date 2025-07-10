bdccf8339600351376299d81e8b9912ecfb1e01d
Matus Caban <caban.matus@gmail.com>
2025-07-10 14:38:28 +0200

    add QuizManager

---
+++ src/main/java/QuizManager.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class QuizManager {
+    private int playerScore;
+    private List<Question> questions;
+
+    //TODO trieda bude manazovat a kontrolovat spravnost odpovedi
+    //TODO bude dalsia trieda ktora bude zbierat a validovat odpovede
+}
