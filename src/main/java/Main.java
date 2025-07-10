ffbe765b20e583453103fd925936ffbbaba8bd18
Matus Caban <caban.matus@gmail.com>
2025-07-10 12:52:27 +0200

    trying reading from json

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,10 @@
+import com.google.gson.Gson;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
 public class Main {
     public static void main(String[] args) {
         String question = "Which is the only continent with land in all four hemispheres?";
@@ -9,5 +16,18 @@ public class Main {
         question1.addAnswer("Europe", false);
 
         question1.printQuestion();
+
+        File file = new File("Question.java");
+        System.out.println(file.getAbsoluteFile());
+
+        Gson gson = new Gson();
+        try (Reader reader = new FileReader("src/json/geography.json")){
+            Question question2 = gson.fromJson(reader, Question.class);
+            question2.printQuestion();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+
     }
 }
